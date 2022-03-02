0fbe2e3a00a20bb97d231915f41bbde910a8e428
Peter Steinbach <p.steinbach@hzdr.de>
2022-03-02 14:32:20 +0100

    refactoring meanstd computation to allow different groupby columns

--- workflow/scripts/mean_std_across_folds.py
+++ workflow/scripts/mean_std_across_folds.py
@@ -5,13 +5,14 @@ import pandas as pd
 from plotnine import *
 
 
-def main(incsv, outcsv):
+def main(incsv, outcsv, groupbycols=["arch", "seed"]):
 
     fulldf = pd.read_csv(incsv)
 
-    gdf = fulldf.groupby(["arch", "seed"])  # should collapse to singular values
+    gdf = fulldf.groupby(groupbycols)  # should collapse to singular values
 
-    print("grouped", gdf.size())
+    print("aggregating mean/std val_accuracy based on col groups", groupbycols)
+    print("grouped df", gdf.size(), "compared to full df", fulldf.size())
     value_accuracy = (
         gdf["val_accuracy"]
         .agg(["mean", "std", "size"])
@@ -25,6 +26,7 @@ def main(incsv, outcsv):
         .reset_index()
     )
 
+    print("aggregating mean/std val_f1score based on col groups", groupbycols)
     value_f1score = (
         gdf["val_f1score"]
         .agg(["mean", "std", "size"])
@@ -41,15 +43,15 @@ def main(incsv, outcsv):
     value = pd.merge(
         value_accuracy,
         value_f1score,
-        on=["arch", "seed"],
+        on=groupbycols,
         how="inner",
         validate="one_to_one",
     )
     # during the analysis, 4 folds yielded 669 samples whereas the remaining 16 670
     # we choose the majority vote here
-    value["nsamples"] = 670
+    value["nsamples"] = fulldf.nsamples.max()
 
-    print(value)
+    print(value.head())
     value.to_csv(str(outcsv), index=False)
 
 
@@ -58,15 +60,25 @@ if __name__ == "__main__":
         hasattr(snakemake, "input") and hasattr(snakemake, "output")
     ):
         opath = Path(snakemake.output[0])
-        value = main(incsv=Path(snakemake.input[0]), outcsv=snakemake.output)
+        groupbycols = ["arch", "seed"]
+        if hasattr(snakemake, "parameters"):
+            groupbycols = snakemake.parameters.groupbycols
+            print(
+                "discovered parameters in snakemake: ", snakemake.parameters.groupbycols
+            )
+        value = main(
+            incsv=Path(snakemake.input[0]),
+            outcsv=snakemake.output,
+            groupbycols=groupbycols,
+        )
         sys.exit(value)
     else:
         assert (
             len(sys.argv) > 2
-        ), f"usage: python plot_seed42_histo.py in.csv to.csv <last|best>"
+        ), f"usage: python mean_std_across_folds.py in.csv to.csv <colon seperated list of columns to groubpy>"
         inputs = sys.argv[1] if len(sys.argv) > 1 else None
-        output = sys.argv[2]
-
-        value = main(incsv=inputs, outcsv=output)
+        output = sys.argv[2] if len(sys.argv) > 2 else "to.csv"
+        groupbycols = sys.argv[3].split(":") if len(sys.argv) > 3 else ["arch", "seed"]
+        value = main(incsv=inputs, outcsv=output, groupbycols=groupbycols)
 
     sys.exit(value)
